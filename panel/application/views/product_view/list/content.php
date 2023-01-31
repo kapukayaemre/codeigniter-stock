2d9bd82c8c00e57e0784859b3922598c153e848d
Emre <kapukayaemre@hotmail.com>
2023-01-31 13:51:40 +0300

    Urunler modulunun urun listesi icin html sablonu olusturuldu

--- panel/application/views/product_view/list/content.php
+++ panel/application/views/product_view/list/content.php
@@ -1,1 +1,50 @@
-Ürün Listesi Content
+<div class="row">
+    <div class="col-md-12">
+        <h4 class="m-b-lg">Tables</h4>
+    </div><!-- END column -->
+
+    <div class="col-md-12">
+        <div class="widget p-lg">
+            <p class="m-b-lg docs">
+                For basic styling—light padding and only horizontal dividers—add the base class <code>.table</code> to
+                any <code>&lt;table&gt;</code>. It may seem super redundant, but given the widespread use of tables for
+                other plugins like calendars and date pickers, we've opted to isolate our custom table styles.
+            </p>
+
+            <table class="table table-hover table-striped">
+                <thead>
+                    <th>#</th>
+                    <th>Ürün Adı</th>
+                    <th>Açıklama</th>
+                    <th>Ana Kategorisi(id)</th>
+                    <th>Alt Kategorisi(id)</th>
+                    <th>Sorumlu</th>
+                    <th>Kayıt Tarihi</th>
+                    <th>Düzenlenme Tarihi</th>
+                    <th>Silinme Tarihi</th>
+                    <th>Durumu</th>
+                    <th>İşlemler</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Masa</td>
+                        <td>4 Kişilik Mutfak Masası</td>
+                        <td>Masalar</td>
+                        <td>Mutfak Masaları</td>
+                        <td>Ahmet Mehmet</td>
+                        <td>31.01.2023</td>
+                        <td>01.02.2023</td>
+                        <td></td>
+                        <td>1/0</td>
+                        <td>
+                            <a href="#" class="btn btn-sm btn-danger btn-outline"><i class="fa fa-trash"></i> Sil </a>
+                            <a href="#" class="btn btn-sm btn-warning btn-outline"><i class="fa fa-pencil-square-o"></i> Düzenle </a>
+                        </td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </div><!-- .widget -->
+    </div>
+</div>
