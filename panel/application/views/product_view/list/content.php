26c18038dcd61675c07d01c3384393283c3fce02
Emre <kapukayaemre@hotmail.com>
2023-01-31 14:13:16 +0300

    Toggle Butonu Eklendi. Yeni Ekle Butonu Eklendi. Kayıt Yoktur Alert Eklendi

--- panel/application/views/product_view/list/content.php
+++ panel/application/views/product_view/list/content.php
@@ -1,16 +1,19 @@
 <div class="row">
     <div class="col-md-12">
-        <h4 class="m-b-lg">Tables</h4>
+        <h4 class="m-b-lg">
+            Ürün Listesi
+            <a href="" class="btn btn-outline btn-primary btn-sm pull-right"><i class="fa fa-plus"></i> Yeni Ekle</a>
+        </h4>
     </div><!-- END column -->
 
     <div class="col-md-12">
         <div class="widget p-lg">
-            <p class="m-b-lg docs">
-                For basic styling—light padding and only horizontal dividers—add the base class <code>.table</code> to
-                any <code>&lt;table&gt;</code>. It may seem super redundant, but given the widespread use of tables for
-                other plugins like calendars and date pickers, we've opted to isolate our custom table styles.
-            </p>
 
+        <div class="alert alert-info text-center">
+                <h5 class="alert-title">Kayıt Bulunamadı</h5>
+                <p>Burada herhangi bir veri bulunmamaktadır. Eklemek için lütfen <a href="#"><strong>tıklayınız.</strong></a></p>
+        </div>
+            
             <table class="table table-hover table-striped">
                 <thead>
                     <th>#</th>
@@ -36,7 +39,9 @@
                         <td>31.01.2023</td>
                         <td>01.02.2023</td>
                         <td></td>
-                        <td>1/0</td>
+                        <td>
+                            <input id="switch-2-2" type="checkbox" data-switchery data-color="#10c469" checked />
+                        </td>
                         <td>
                             <a href="#" class="btn btn-sm btn-danger btn-outline"><i class="fa fa-trash"></i> Sil </a>
                             <a href="#" class="btn btn-sm btn-warning btn-outline"><i class="fa fa-pencil-square-o"></i> Düzenle </a>
